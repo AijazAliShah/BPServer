76292e46f315992e1a94ed110a58893431ab55c7
aijazalishah <aijazalishah@live.com>
2021-12-26 20:25:42 +0500

    mongochanged

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     mongoURI:
-      "mongodb+srv://Bp_db:bp_db@cluster0-eigwa.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority",
+      // "mongodb+srv://Bp_db:bp_db@cluster0-eigwa.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority",
+      "mongodb+srv://bp:bp@cluster0.cu55o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
       secretOrkey: "secret",
 };
